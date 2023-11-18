e13695faeb976ada783acc5cf5abe6cea41bc083
choonghyun-park <choonghyun.park10@gmail.com>
2023-11-18 19:40:46 +0900

    update

--- solved.ac/11724.cpp
+++ solved.ac/11724.cpp
@@ -1,8 +1,12 @@
 #include <iostream>
 #include <vector>
 #include <map>
+#include <queue>
+#include <set>
 
 using namespace std;
+
+void make_edge(map<int,vector<int>> &graph, int key, int value);
 
 int main() {
     int N,M;
@@ -16,10 +20,62 @@ int main() {
     for (int i=0;i<M;i++){
         int u,v; // 간선의 양 끝점
         cin >> u >> v;
-        if (graph.find(u))
+        make_edge(graph,u,v);
+        make_edge(graph,v,u);
     }
 
+    /* test print
+    cout << "graph info" << endl;
+    for (auto it=graph.begin();it!=graph.end();it++){
+        cout << it->first << " : ";
+        auto vec = it->second;
+        for (auto iter=vec.begin();iter!=vec.end();iter++){
+            cout << *iter << ' ';
+        }
+        cout << endl;
+    }    
+    */
 
+    int connects_cnt = 0;
+    set<int> visited;
+    
+    for (int i=1;i<=N;i++){
+        int init_node = i;
+        if (visited.find(init_node)!=visited.end()){
+            continue;
+        }
 
+        queue<int> q;
+        q.push(init_node);
+        set<int> connects;
+        // BFS search
+        while(!q.empty()){
+            int node = q.front();
+            q.pop();
+            visited.insert(node);            
+            connects.insert(node);
+            vector<int> neighbors = graph[node];
+            for (auto it=neighbors.begin();it!=neighbors.end();it++){
+                int next_node = *it;
+                if (connects.find(next_node)==connects.end()){
+                    q.push(next_node);          
+                }
+            }
+        }
+        connects_cnt++;
+    }
+    cout << connects_cnt << endl;
+}
 
+void make_edge(map<int,vector<int>> &graph, int key, int value){
+    if (graph.find(key)==graph.end()){ // key가 존재하지 않음
+            vector<int> vec;
+            vec.push_back(value);
+            graph[key] = vec;
+    }
+    else{ // key가 존재함
+        vector<int> vec = graph[key];
+        vec.push_back(value);
+        graph[key] = vec;
+    }
 }
